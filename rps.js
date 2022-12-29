71a4a34a742588387779eb1afdacc2e7da13f880
YasmineRico <ricolyasmine@gmail.com>
2022-12-28 19:21:55 -0600

    create function for a single round of Rock paper scissors, create variable for playerSelection and computer selection, have results in the console

--- rps.js
+++ rps.js
@@ -1,6 +1,30 @@
 //Create array that has the choices for RPS game
 let gameArr = ['rock', 'paper', 'scissors'];
 
-let computerChoice = gameArr[Math.floor(Math.random()*gameArr.length)];
+//Variable to return a random element from the gameArr array
+let getComputerChoice = gameArr[Math.floor(Math.random()*gameArr.length)];
 
-console.log(computerChoice);
+
+
+//function to compare computer selection and player selection to determine win/lose/tie
+function singleRound (playerselection, computerSelection){
+    if (playerselection === 'rock' && computerSelection === 'scissors' 
+    || playerselection == 'scissors' && computerSelection === 'paper' 
+    || playerselection === 'paper' && computerSelection === 'rock' ){
+        return "Player wins"
+    }else if (playerselection === computerSelection){
+        return "Tie!";
+    }else
+        return "Machine Wins!";
+}
+
+//variable for player selection
+const playerselection = 'paper';
+
+//variable for computer selection
+const computerSelection =getComputerChoice;
+
+
+console.log(playerselection);
+console.log(computerSelection);
+console.log(singleRound(playerselection, computerSelection));
